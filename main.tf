633cbb55cf311724223a0c59ef479b18f26d1968
drLee0011 <155828057+drLee0011@users.noreply.github.com>
2024-11-10 03:54:22 +0000

    main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ data "aws_internet_gateway" "existing_gw" {
 
 # Create a new Internet Gateway if none exists
 resource "aws_internet_gateway" "gw" {
-  count   = length(data.aws_internet_gateway.existing_gw.ids) == 0 ? 1 : 0
+  count   = data.aws_internet_gateway.existing_gw.id == "" ? 1 : 0
   vpc_id  = data.aws_vpc.main.id
 }
 
@@ -52,7 +52,7 @@ resource "aws_route_table" "public_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = length(data.aws_internet_gateway.existing_gw.ids) == 0 ? aws_internet_gateway.gw[0].id : data.aws_internet_gateway.existing_gw.ids[0]
+    gateway_id = data.aws_internet_gateway.existing_gw.id != "" ? data.aws_internet_gateway.existing_gw.id : aws_internet_gateway.gw[0].id
   }
 
   tags = {
